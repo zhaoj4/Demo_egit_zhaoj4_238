cb55b65fe95069ebbf2850f3a446cfe8824a589f
zhaoj4 <zhaoj4@emc.com>
2014-03-06 21:20:41 -0800

    commit an Eclipse project into git

---
+++ src/helloworld_git.java
@@ -0,0 +1,15 @@
+/**
+ * edit a project in git
+ * @author zhaoj4
+ *
+ */
+
+public class helloworld_git {
+
+	public static void main(String[] args) {
+		// a testing eclipse project into git
+		System.out.println("Hello World for git");
+
+	}
+
+}
